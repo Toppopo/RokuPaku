a0188fee394c581642fa7f2816ca528117c89301
PG22-01 <>
2024-02-22 15:01:49 +0900

    wakaran

--- Assets/ONO/Scripts/EnemyDestroy/EnemyDestroy.cs
+++ Assets/ONO/Scripts/EnemyDestroy/EnemyDestroy.cs
@@ -16,12 +16,6 @@ public class EnemyDestroy : MonoBehaviour
     private Vector3 initialPosition; // ‰ŠúˆÊ’u
     private bool reverseDirection = false; // •ûŒü”½“]ƒtƒ‰ƒO
 
-
-    /*[SerializeField] private Transform _LeftEdge;
-    [SerializeField] private Transform _RightEdge;
-    private float MoveSpeed = 3.0f;
-    private int direction = 1;*/
-
     private Slash slash;
 
     void Start()
@@ -59,12 +53,4 @@ public class EnemyDestroy : MonoBehaviour
             }
         }
     }
-    /*void FixedUpdate()
-    {
-        if (transform.position.x >= _RightEdge.position.x)
-            direction = -1;
-        if (transform.position.x <= _LeftEdge.position.x)
-            direction = 1;
-        transform.position = new Vector3(transform.position.x + MoveSpeed * Time.fixedDeltaTime * direction, -4, 0);
-    }*/
 }
